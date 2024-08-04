b59f9e077c5a192e7999cb2b693f1a5a46e37e19
Swarup Saha <swarupsaha@Swarups-iMac-Pro.local>
2024-08-05 04:52:53 +0530

    first commit

---
+++ mlzone/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mlzone project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mlzone.settings")
+
+application = get_asgi_application()
